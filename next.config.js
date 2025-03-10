c9fa76bbc130c57bd0cb2e97d9afea1d51f48e52
Sathish Kumar <voltora13@gmail.com>
2025-03-10 21:56:27 +0530

    fix: update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    reactStrictMode: true,
-    images: {
-      unoptimized: true, // Required for 'output: export'
-    },
   };
   
   module.exports = nextConfig;
